7db373363127196671a9c2708e8730899e60985e
fernandoludvig <fernandoludvig0804@gmail.com>
2024-10-03 20:54:32 -0300

    erro

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const app = express();
 const static = require("./routes/static");
 const baseController = require("./controllers/baseController");
 const inventoryRoute = require("./routes/inventoryRoute"); // Import inventory route
+const utilities = require("./utilities/"); // Certifique-se de que o arquivo utilities está sendo importado
 
 /* ***********************
  * View Engine and Templates
@@ -25,10 +26,40 @@ app.set("layout", "./layouts/layout"); // Not at views root
  * Routes
  *************************/
 app.use(static);
+
 // Index route
-app.get("/", baseController.buildHome);
+app.get("/", utilities.handleErrors(baseController.buildHome)); // Wrap with error handling
+
 // Inventory routes
 app.use("/inv", inventoryRoute);
+
+/* ***********************
+ * Express Error Handler
+ * Place after all other middleware
+ *************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav();
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`);
+  
+  // Check for 404 status code
+  let message;
+  if (err.status === 404) {
+    message = err.message;
+  } else {
+    message = 'Oh no! There was a crash. Maybe try a different route?';
+  }
+  
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  });
+});
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({ status: 404, message: 'Sorry, we appear to have lost that page.' });
+});
 
 /* ***********************
  * Local Server Information
--- utilities/index.js
+++ utilities/index.js
@@ -56,4 +56,11 @@ Util.buildClassificationGrid = async function(data) {
   return grid; // Return the constructed HTML string
 }
 
+/* ****************************************
+ * Middleware For Handling Errors
+ * Wrap other function in this for 
+ * General Error Handling
+ **************************************** */
+Util.handleErrors = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next)
+
 module.exports = Util; // Export utility functions
